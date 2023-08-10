4e0f80343714f983e77cc3484277cfab00a47f67
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-09 19:28:04 -0500

    Add VM resize script.

---
+++ scripts/resize-vm.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+CURRENT_SCRIPT_DIR="$(dirname "$0")/";
+echo "Running ${0} from ${CURRENT_SCRIPT_DIR}";
+echo;
+
+# Import library functions
+source ${CURRENT_SCRIPT_DIR}lib/azure-cli-functions.sh
+
+print-usage () {
+  echo "This script requires three parameters:";
+  echo "  - resource group name";
+  echo "  - server name";
+  echo "  - size";
+  echo;
+}
+
+parse-script-inputs () {
+  if [ "$#" -ne 3 ]
+    then
+      print-usage;
+      exit 1;
+    else
+      RESOURCE_GROUP="$1";
+      SERVER_NAME="$2";
+      VM_SIZE="$3";
+  fi
+}
+
+resize-vm () {
+  echo "Resizing virtual machine...";
+
+  az vm resize \
+    --resource-group $RESOURCE_GROUP \
+    --name $NSG_NAME \
+    --size $VM_SIZE;
+
+  echo "---";
+  echo;
+}
+
+main () {
+  parse-script-inputs $@;
+  validate-az-cli-install;
+  check-signed-in-user;
+
+  resize-vm;
+
+  echo "---";
+  echo "Done.";
+  echo;
+}
+
+time main $@;
