c5fe1902d22c95483f88416caebf7e73384d1905
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-10 19:13:53 -0500

    Expand for powershell env setup and public key auth.

--- powershell/install-ssh.ps1
+++ powershell/install-ssh.ps1
@@ -1,3 +1,29 @@
+# Helper function for MSI installations
+function Install-Msi {
+    param(
+        $File
+    )
+
+    $DataStamp = get-date -Format yyyyMMddTHHmmss
+    $logFile = '{0}-{1}.log' -f $File.fullname,$DataStamp
+    $MSIArguments = @(
+        "/i"
+        ('"{0}"' -f $File.fullname)
+        "/qn"
+        "/norestart"
+        "/L*v"
+        $logFile
+    )
+    Start-Process "msiexec.exe" -ArgumentList $MSIArguments -Wait -NoNewWindow 
+}
+
+$ProgressPreference = 'SilentlyContinue'
+
+# Download and Install Powershell Environment
+Invoke-WebRequest -Uri https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/PowerShell-7.3.6-win-x64.msi -OutFile ~/powershell.msi -UseBasicParsing
+$msiFile = Get-ChildItem -Path ~/powershell.msi
+Install-Msi -File $msiFile
+
 # Install the OpenSSH Client
 Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
 
@@ -15,3 +41,45 @@ if (!(Get-NetFirewallRule -Name "OpenSSH-Server-In-TCP" -ErrorAction SilentlyCon
 } else {
     Write-Output "Firewall rule 'OpenSSH-Server-In-TCP' has been created and exists."
 }
+
+# Add Powershell Subsystem and Public Key Auth to sshd_config
+$sshdConfigFile = Get-ChildItem -Path $env:ProgramData/ssh/sshd_config
+$subSystemLine = "Subsystem powershell c:/progra~1/powershell/7/pwsh.exe -sshs -nologo"
+$pubKeyAuthLine = "PubkeyAuthentication yes"
+@($subSystemLine) + (Get-Content $sshdConfigFile.FullName) | Set-Content $sshdConfigFile.FullName
+@($pubKeyAuthLine) + (Get-Content $sshdConfigFile.FullName) | Set-Content $sshdConfigFile.FullName
+
+# Create and configure administrators_authorized_keys file
+# with permissions that sshd will accept
+$authorizedKeyFile = "c:\ProgramData\ssh\administrators_authorized_keys"
+New-Item -Path $authorizedKeyFile -ItemType file -Force
+
+$NewAcl = Get-Acl -Path $authorizedKeyFile
+
+# disable permission inheritance
+$NewAcl.SetAccessRuleProtection($true, $false)
+
+# Set properties for SYSTEM
+$identity = "SYSTEM"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+# Create new rule
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+# Apply new rule
+$NewAcl.SetAccessRule($fileSystemAccessRule)
+
+# Set properties for administrators
+$identity = "BUILTIN\Administrators"
+$fileSystemRights = "FullControl"
+$type = "Allow"
+# Create new rule
+$fileSystemAccessRuleArgumentList = $identity, $fileSystemRights, $type
+$fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule -ArgumentList $fileSystemAccessRuleArgumentList
+# Apply new rule
+$NewAcl.SetAccessRule($fileSystemAccessRule)
+
+# Apply ACL
+Set-Acl -Path $authorizedKeyFile -AclObject $NewAcl
+
+Restart-Service sshd
