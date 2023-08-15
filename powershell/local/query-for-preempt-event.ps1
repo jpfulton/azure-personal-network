9e778b3afbe89c05ddb612347b0c77b3b33d095b
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-15 19:15:30 +0000

    Update with full shutdown logic.

--- powershell/local/query-for-preempt-event.ps1
+++ powershell/local/query-for-preempt-event.ps1
@@ -7,6 +7,9 @@ $API_VERSION="2020-07-01"
 $HEADERS=@{ "Metadata" = "true" }
 $ENDPOINT_URL="http://${ENDPOINT_IP}/metadata/scheduledevents?api-version=${API_VERSION}"
 
+$NOTIFIER_CLI="C:\Users\jpfulton\AppData\Local\Yarn\bin\sms-notify-cli.cmd"
+$HOSTNAME=$(hostname)
+
 Write-Host "Calling Azure Metadata API endpoint..."
 $jsonOutput = Invoke-RestMethod -Method Get -Uri $ENDPOINT_URL -Headers $HEADERS | ConvertFrom-Json -Depth 64
 $output = ConvertFrom-Json -InputObject $jsonOutput
@@ -18,11 +21,25 @@ if ($output.Events.Length -gt 0) {
     if ($event.EventType -eq "Preempt") {
       Write-Host "Preempt event found. Starting graceful shutdown..."
 
+      # Write eviction discovery to system event log
+      Write-EventLog `
+        -EventId 100 `
+        -LogName "Application" `
+        -Source "Azure Spot Instance Eviction Service" `
+        -EntryType Warning `
+        -Message "Azure eviction event discovered. Starting gracecful shutdown..."
+
       # Message logged in users
       msg * "Azure spot instance eviction detected. Gracefull shutdown starting..."
+
+      # Send SMS notification to administrators
+      & $NOTIFIER_CLI eviction $HOSTNAME
+
+      # Sleep for 5 seconds
       Start-Sleep 5
 
-      
+      # Initiate shutdown
+      Stop-Computer -Force
     }
   }
 }
