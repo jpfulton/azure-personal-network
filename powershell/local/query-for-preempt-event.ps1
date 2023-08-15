c0b83221a90edfaed8c1c7f3d2d8be697f3a5074
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-08-15 18:30:00 -0500

    Wrap core logic in try block and exit with error code.

--- powershell/local/query-for-preempt-event.ps1
+++ powershell/local/query-for-preempt-event.ps1
@@ -18,48 +18,58 @@ function Add-ToLogFile () {
   Add-Content -Path $LOG_FILE -Value $logLine
 }
 
-Add-ToLogFile -Content "Calling Azure Metadata API endpoint..."
-$jsonOutput = Invoke-RestMethod -Method Get -Uri $ENDPOINT_URL -Headers $HEADERS | ConvertTo-Json -Depth 64
-$output = ConvertFrom-Json -InputObject $jsonOutput
+try {
+  $ErrorActionPreference = 'Stop'
 
-if ($output.Events.Length -gt 0) {
-  Add-ToLogFile -Content "Azure event(s) found... Looking for Preempt events..."
+  Add-ToLogFile -Content "Calling Azure Metadata API endpoint..."
+  $jsonOutput = Invoke-RestMethod -Method Get -Uri $ENDPOINT_URL -Headers $HEADERS | ConvertTo-Json -Depth 64
+  $output = ConvertFrom-Json -InputObject $jsonOutput
 
-  foreach($event in $output.Events) {
-    if ($event.EventType -eq "Preempt") {
-      Add-ToLogFile -Content "Preempt event found. Starting graceful shutdown..."
+  if ($output.Events.Length -gt 0) {
+    Add-ToLogFile -Content "Azure event(s) found... Looking for Preempt events..."
 
-      # Send SMS notification to administrators
-      Add-ToLogFile -Content "Sending SMS notifications."
-      $smsJob = Start-Job -ScriptBlock { 
-        $NOTIFIER_CLI="C:\Users\jpfulton\AppData\Local\Yarn\bin\sms-notify-cli.cmd"
-        $HOSTNAME=$(hostname)
-        & $NOTIFIER_CLI eviction $HOSTNAME 
+    foreach($event in $output.Events) {
+      if ($event.EventType -eq "Preempt") {
+        Add-ToLogFile -Content "Preempt event found. Starting graceful shutdown..."
+
+        # Send SMS notification to administrators
+        Add-ToLogFile -Content "Sending SMS notifications."
+        $smsJob = Start-Job -ScriptBlock { 
+          $NOTIFIER_CLI="C:\Users\jpfulton\AppData\Local\Yarn\bin\sms-notify-cli.cmd"
+          $HOSTNAME=$(hostname)
+          & $NOTIFIER_CLI eviction $HOSTNAME 
+        }
+
+        # Message logged in users
+        Add-ToLogFile -Content "Messaging logged in users."
+        $msgJob = Start-Job -ScriptBlock { 
+          msg * "Azure spot instance eviction detected. Graceful shutdown starting..." 
+        }
+
+        # Write eviction discovery to system event log
+        Add-ToLogFile -Content "Writing to event log."
+        $eventLogJob = Start-Job -ScriptBlock {
+          Write-EventLog `
+          -EventId 100 `
+          -LogName "Application" `
+          -Source "Azure Spot Instance Eviction Service" `
+          -EntryType Warning `
+          -Message "Azure eviction event discovered. Starting gracecful shutdown..."
+        }
+
+        # wait on background jobs
+        Wait-Job -Job @($smsJob, $msgJob, $eventLogJob)
+
+        # Initiate shutdown
+        Add-ToLogFile -Content "Shutting down."
+        Stop-Computer -Force
       }
-
-      # Message logged in users
-      Add-ToLogFile -Content "Messaging logged in users."
-      $msgJob = Start-Job -ScriptBlock { 
-        msg * "Azure spot instance eviction detected. Graceful shutdown starting..." 
-      }
-
-      # Write eviction discovery to system event log
-      Add-ToLogFile -Content "Writing to event log."
-      $eventLogJob = Start-Job -ScriptBlock {
-        Write-EventLog `
-        -EventId 100 `
-        -LogName "Application" `
-        -Source "Azure Spot Instance Eviction Service" `
-        -EntryType Warning `
-        -Message "Azure eviction event discovered. Starting gracecful shutdown..."
-      }
-
-      # wait on background jobs
-      Wait-Job -Job @($smsJob, $msgJob, $eventLogJob)
-
-      # Initiate shutdown
-      Add-ToLogFile -Content "Shutting down."
-      Stop-Computer -Force
     }
   }
+}
+catch {
+  Exit 1
+}
+finally {
+  $ErrorActionPreference = 'Continue'
 }
