9b40bbe54ca1f9362db4529e003cf372bc8833ac
J. Patrick Fulton <jpatrick.fulton@gmail.com>
2023-09-13 21:29:39 -0500

    Pass env to sudo commands.

--- linux/core/setup-node-and-yarn.sh
+++ linux/core/setup-node-and-yarn.sh
@@ -28,8 +28,8 @@ setup-nodejs () {
       NODE_MAJOR=18;
       echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list;
 
-      sudo apt-get update;
-      sudo apt-get install nodejs -y;
+      sudo -E apt-get update;
+      sudo -E apt-get install nodejs -y;
   fi
 
   echo "---";
